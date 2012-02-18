9892fda2bc5f27e1f0fd9af35fdc9f98b366e790
urbanr <radovan.urban@gmail.com>
2012-02-18 13:38:41 +0100

    Test commit. Pridany testovaci komentar

--- src/cz/leftovers/geocatching/GeocatchingActivity.java
+++ src/cz/leftovers/geocatching/GeocatchingActivity.java
@@ -2,7 +2,9 @@ package cz.leftovers.geocatching;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/*
+ * Comment
+ */
 public class GeocatchingActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
