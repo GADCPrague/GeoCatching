66696704f1f74116f82eb388d113aceba473f834
urbanr <radovan.urban@gmail.com>
2012-02-18 16:20:15 +0100

    Changes in GEocatchingActivity added.

--- src/cz/leftovers/geocatching/GeocatchingActivity.java
+++ src/cz/leftovers/geocatching/GeocatchingActivity.java
@@ -17,7 +17,7 @@ public class GeocatchingActivity extends Activity {
 	 * Starts this CommanderAppActivity.
 	 * @param savedInstanceState
 	 */
-	private int timerValue = 5000;
+	private int timerValue = 0000;
 	private TextView time;
 	
 	@Override
