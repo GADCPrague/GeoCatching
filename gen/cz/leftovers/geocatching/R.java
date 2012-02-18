9a47678b6bde13be7864fdfb18d6ed907ea3be60
urbanr <radovan.urban@gmail.com>
2012-02-18 16:45:10 +0100

    MainMenuLayout upraven

--- gen/cz/leftovers/geocatching/R.java
+++ gen/cz/leftovers/geocatching/R.java
@@ -10,20 +10,54 @@ package cz.leftovers.geocatching;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int title_background_color=0x7f040000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_case_off=0x7f020000;
+        public static final int ic_case_on=0x7f020001;
+        public static final int ic_examine_off=0x7f020002;
+        public static final int ic_examine_on=0x7f020003;
+        public static final int ic_games_off=0x7f020004;
+        public static final int ic_games_on=0x7f020005;
+        public static final int ic_games_shine=0x7f020006;
+        public static final int ic_go_off=0x7f020007;
+        public static final int ic_go_on=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int ic_mouth_off=0x7f02000a;
+        public static final int ic_mouth_on=0x7f02000b;
+        public static final int ic_refresh_off=0x7f02000c;
+        public static final int ic_refresh_on=0x7f02000d;
+        public static final int selector_btn_games=0x7f02000e;
     }
     public static final class id {
-        public static final int checkBox_remember=0x7f050005;
-        public static final int games_listview=0x7f050000;
-        public static final int login_button=0x7f050006;
-        public static final int login_label=0x7f050001;
-        public static final int login_text=0x7f050002;
-        public static final int my_games_listview=0x7f050009;
-        public static final int password_label=0x7f050003;
-        public static final int password_text=0x7f050004;
-        public static final int register_button=0x7f050007;
-        public static final int timer_label=0x7f050008;
+        public static final int buttonExit=0x7f060019;
+        public static final int buttonGameList=0x7f06000d;
+        public static final int buttonLogo=0x7f06000a;
+        public static final int buttonPlayerGameList=0x7f06000f;
+        public static final int buttonPlayerHistory=0x7f060012;
+        public static final int buttonSettings=0x7f060014;
+        public static final int buttonTMP=0x7f060017;
+        public static final int checkBox_remember=0x7f060005;
+        public static final int frameLayout1=0x7f06000c;
+        public static final int frameLayout2=0x7f06000e;
+        public static final int frameLayout3=0x7f060011;
+        public static final int frameLayout4=0x7f060013;
+        public static final int frameLayout5=0x7f060016;
+        public static final int frameLayout6=0x7f060018;
+        public static final int games_listview=0x7f060000;
+        public static final int layout=0x7f060015;
+        public static final int layoutButtonRow1=0x7f06000b;
+        public static final int layoutButtonRow2=0x7f060010;
+        public static final int layoutTopStrip=0x7f060009;
+        public static final int login_button=0x7f060006;
+        public static final int login_label=0x7f060001;
+        public static final int login_text=0x7f060002;
+        public static final int my_games_listview=0x7f06001a;
+        public static final int password_label=0x7f060003;
+        public static final int password_text=0x7f060004;
+        public static final int register_button=0x7f060007;
+        public static final int timer_label=0x7f060008;
     }
     public static final class layout {
         public static final int game_list_layout=0x7f030000;
@@ -33,15 +67,15 @@ public final class R {
         public static final int my_game_list_layout=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int close=0x7f040009;
-        public static final int error=0x7f040007;
-        public static final int hello=0x7f040000;
-        public static final int login=0x7f040002;
-        public static final int login_button=0x7f040005;
-        public static final int password=0x7f040003;
-        public static final int register_button=0x7f040006;
-        public static final int remember_login=0x7f040004;
-        public static final int wrong_parameters=0x7f040008;
+        public static final int app_name=0x7f050001;
+        public static final int close=0x7f050009;
+        public static final int error=0x7f050007;
+        public static final int hello=0x7f050000;
+        public static final int login=0x7f050002;
+        public static final int login_button=0x7f050005;
+        public static final int password=0x7f050003;
+        public static final int register_button=0x7f050006;
+        public static final int remember_login=0x7f050004;
+        public static final int wrong_parameters=0x7f050008;
     }
 }
