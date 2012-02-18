d9bc9fdb05ce4f6263395730d6d09267db77a95a
Michal Svacha <svacha@msn.com>
2012-02-18 14:05:41 +0100

    Prvni kostra pro splashscreen a mainmenu activity

---
+++ src/cz/leftovers/geocatching/GameListActivity.java
@@ -0,0 +1,13 @@
+package cz.leftovers.geocatching;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class GameListActivity extends ListActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
