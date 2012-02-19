e38b003e42a5914c29ad584d86cec179f6ffddcb
Michal Svacha <svacha@msn.com>
2012-02-19 15:36:19 +0100

    Listenery pro ikony v settings

--- src/cz/leftovers/geocatching/SettingsActivity.java
+++ src/cz/leftovers/geocatching/SettingsActivity.java
@@ -2,6 +2,10 @@ package cz.leftovers.geocatching;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
@@ -10,5 +14,37 @@ public class SettingsActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_layout);
+		
+		ImageView iv = (ImageView) this.findViewById(R.id.fb_icon);
+		iv.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SettingsActivity.this, "Facebook linking window", Toast.LENGTH_LONG);
+			}
+		});
+		
+		iv = (ImageView) this.findViewById(R.id.gplus_icon);
+		iv.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SettingsActivity.this, "Google+ linking window", Toast.LENGTH_LONG);
+			}
+		});
+		
+		iv = (ImageView) this.findViewById(R.id.tw_icon);
+		iv.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SettingsActivity.this, "Twitter linking window", Toast.LENGTH_LONG);
+			}
+		});
+		
+		iv = (ImageView) this.findViewById(R.id.fsq_icon);
+		iv.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SettingsActivity.this, "Foursquare linking window", Toast.LENGTH_LONG);
+			}
+		});
 	}
 }
