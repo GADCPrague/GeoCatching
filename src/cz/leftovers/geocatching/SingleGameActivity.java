56d6170cdf9192fb74ddda73b158a7e453a73a45
Michal Svacha <svacha@msn.com>
2012-02-19 15:59:50 +0100

    resolved

--- src/cz/leftovers/geocatching/SingleGameActivity.java
+++ src/cz/leftovers/geocatching/SingleGameActivity.java
@@ -2,12 +2,28 @@ package cz.leftovers.geocatching;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
 
 public class SingleGameActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.single_game_activity_layout);
+		setContentView(R.layout.single_game_layout);
+		
+		Button btn = (Button) this.findViewById(R.id.button1);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				DialogPrey ad = new DialogPrey(SingleGameActivity.this);
+				ad.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				ad.show();
+				
+			}
+		});
 	}
 
 }
