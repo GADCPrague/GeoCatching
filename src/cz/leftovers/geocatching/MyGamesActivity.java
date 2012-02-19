e3ad90b5f076c707580c21e514b07a095d5e5420
urbanr <radovan.urban@gmail.com>
2012-02-19 13:49:47 +0100

    Ikony twitter,facebook,twitter,g+

--- src/cz/leftovers/geocatching/MyGamesActivity.java
+++ src/cz/leftovers/geocatching/MyGamesActivity.java
@@ -121,27 +121,36 @@ public class MyGamesActivity extends Activity {
 				sView = new SingleGameView();
 				
 				sView.name = (TextView)rowView.findViewById(R.id.game_list_item_main_text);
-				sView.location = (TextView)rowView.findViewById(R.id.game_list_item_location);
-				sView.players = (TextView)rowView.findViewById(R.id.game_list_item_players);
-				sView.date = (TextView)rowView.findViewById(R.id.game_list_item_date);
+				sView.location_label = (TextView)rowView.findViewById(R.id.game_list_item_location_label);
+				sView.location_value = (TextView)rowView.findViewById(R.id.game_list_item_location_value);
+				sView.players_label = (TextView)rowView.findViewById(R.id.game_list_item_players_label);
+				sView.players_value = (TextView)rowView.findViewById(R.id.game_list_item_players_value);
+				sView.date_label = (TextView)rowView.findViewById(R.id.game_list_item_date_label);
+				sView.date_value = (TextView)rowView.findViewById(R.id.game_list_item_date_value);
 
 				rowView.setTag(sView);
 			} else {
 				sView = (SingleGameView) rowView.getTag();
 			}
 			sView.name.setText(objects.get(position).name);
-			sView.location.setText( getString(R.string.game_list_item_location) + ": " + objects.get(position).area);
-			sView.players.setText( getString(R.string.game_list_item_players) + ": " + 0 + "/" + objects.get(position).capacity);
-			sView.date.setText( getString(R.string.game_list_item_date) + ": " + objects.get(position).gamefrom + "-" + objects.get(position).gameto);
+			sView.location_label.setText( getString(R.string.game_list_item_location) + ": " );
+			sView.location_value.setText( objects.get(position).area );
+			sView.players_label.setText( getString(R.string.game_list_item_players) + ": " );
+			sView.players_value.setText( 0 + "/" + objects.get(position).capacity);
+			sView.date_label.setText( getString(R.string.game_list_item_date) + ": ");
+			sView.date_value.setText( objects.get(position).gamefrom + "-" + objects.get(position).gameto);
 			
 			return rowView;
 		}
 		
 		protected class SingleGameView {
 			protected TextView name;
-			protected TextView location;
-			protected TextView players;
-			protected TextView date;
+			protected TextView location_label;
+			protected TextView location_value;
+			protected TextView players_label;
+			protected TextView players_value;
+			protected TextView date_label;
+			protected TextView date_value;
 		}
 	}
 
