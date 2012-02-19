e4d046d9acd34a17bb447fd7b00a31e35aeade30
Michal Svacha <svacha@msn.com>
2012-02-19 15:46:32 +0100

    yml na single game

--- src/cz/leftovers/geocatching/MainMenuActivity.java
+++ src/cz/leftovers/geocatching/MainMenuActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -63,6 +64,9 @@ public class MainMenuActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				Uri uri = Uri.parse("https://plus.google.com/104035698554795792677/posts?hl=cs");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(intent);
 			}
 		});
 		
